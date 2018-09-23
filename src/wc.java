918afb3e00f47be8179fbfe510c970f038e2967c
ChrisVejsada <chrisjvejsada@gmail.com>
2018-09-23 00:52:52 -0700

    Adding Utils

--- src/wc.java
+++ src/wc.java
@@ -1,4 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+
 public class wc {
+    private static final int
     public static void main(String[] args){
 
     }
